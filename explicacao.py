1e884c5ec2d9411742e4034d3bea3c695531424f
Wagner Filho <81321824+wagnerfilh@users.noreply.github.com>
2023-12-04 01:18:11 -0300

    Updating explanatory menu text lines
    .

--- explicacao.py
+++ explicacao.py
@@ -12,16 +12,16 @@ def iniciar_explicacao():
     rodar = True
 
     textos = [
-        "(1) constante - algoritmos que executam na mesma velocidade ex: stack.push()",
-        "(log n) logarítimo iterado - executam uma fração do comprimento dos dados ex: binary search",
-        "(n) linear -  linear search, max(n1, n2)",
-        "(n * log n) logarítmica - ",
-        "(n^2) quadrática - selection sort, bubble sort",
-        "(n^3) cúbica - multiplicação de matrizes",
-        "(B^n) exponencial - ",
-        "NP Not Only Polynomial - Algoritmos que ainda",
-        "NP Hard -",
-        "NP Complete -",
+    "(1) constante - algoritmos que executam na mesma velocidade ex: stack.push()",
+    "(log n) logarítimo iterado - executam uma fração do comprimento dos dados ex: binary search",
+    "(n) linear -  linear search, max(n1, n2)",
+    "(n * log n) logarítmica - algoritmos eficientes em dados ordenados ex: merge sort",
+    "(n^2) quadrática - algoritmos ineficientes para grandes conjuntos de dados ex: selection sort, bubble sort",
+    "(n^3) cúbica - multiplicação de matrizes",
+    "(B^n) exponencial - algoritmos com crescimento exponencial em relação ao tamanho dos dados",
+    "NP Not Only Polynomial - Algoritmos que ainda não têm solução polinomial conhecida",
+    "NP Hard - problemas que são pelo menos tão difíceis quanto os mais difíceis em NP",
+    "NP Complete - problemas que são tanto NP quanto NP Hard",
     ]
     
     while rodar:
